08869a03f54be58da413e45b4d5863bcba52bf22
jimchen5209 <jimchen5209@gmail.com>
2022-11-01 17:00:08 +0800

    fix: add missing update source list step

--- Ubuntu/install-base.sh
+++ Ubuntu/install-base.sh
@@ -157,6 +157,9 @@ install-docker() {
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+    echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Update apt source list${CLEAR}"
+    sudo apt update
+
     echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Install docker${CLEAR}"
     sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
