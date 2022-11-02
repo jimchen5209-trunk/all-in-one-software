6f9384b1f53a34603e140430f40710ecebd02e2b
jimchen5209 <jimchen5209@gmail.com>
2022-11-02 11:39:33 +0800

    feat: add action to fix zsh on kubuntu

--- Ubuntu/extras/install-kubuntu.sh
+++ Ubuntu/extras/install-kubuntu.sh
@@ -12,3 +12,20 @@ CLEAR='\033[0m'   # Clear color and formatting
 # Startup
 echo -e "${BLUE}${BOLD}=> ${WHITE}Extra config for Kubuntu${CLEAR}"
 
+fix-zsh() {
+    echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Add Profile to ZProfile${CLEAR}"
+    echo "emulate sh -c 'source /etc/profile'" | sudo tee -a /etc/zsh/zprofile > /dev/null
+
+    echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Fix nvm path for desktop app${CLEAR}"
+    echo "export NVM_DIR=\"\$([ -z \"\${XDG_CONFIG_HOME-}\" ] && printf %s \"\${HOME}/.nvm\" || printf %s \"\${XDG_CONFIG_HOME}/nvm\")" >> ~/.zprofile
+    echo "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\" # This loads nvm" >> ~/.zprofile
+}
+
+install-all() {
+    echo -e "\n${GREEN}${BOLD}SETUP ${BLUE}=> ${CYAN}Fix Z-Shell for Kubuntu${CLEAR}"
+    fix-zsh
+}
+
+install-all
+
+echo -e "\n${BLUE}${BOLD}=> ${WHITE}Extra config for Kubuntu Install Complete!${CLEAR}\n"
