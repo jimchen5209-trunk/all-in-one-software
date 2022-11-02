4f144db2621800186ab17d91cbebbf8220cf8891
jimchen5209 <jimchen5209@gmail.com>
2022-11-02 10:25:51 +0800

    refactor: unify gpg path

--- Ubuntu/install-base.sh
+++ Ubuntu/install-base.sh
@@ -153,9 +153,9 @@ install-docker() {
     sudo apt -y install ca-certificates curl gnupg lsb-release
 
     echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Add repository${CLEAR}"
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo mkdir -p /etc/apt/trusted.gpg.d
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     echo -e "\n${YELLOW}${BOLD}STEP ${BLUE}=> ${WHITE}Update apt source list${CLEAR}"
     sudo apt update
